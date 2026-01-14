4554f7a3d8a3be14cb2d9c80693a8bb218a42b60
Harish <harish@Mac.fritz.box>
2026-01-14 07:47:13 +0100

    :construction: Basic Calendar with Day Highlight

---
+++ src/data/journal.js
@@ -0,0 +1,11 @@
+const JOURNAL = {
+     "2026-01-06": {
+    text: "My first flight",
+    image: "/images/flight.png",
+
+    bubbleOffset: { x: -40, y: -120 },
+    imageOffset: { x: 40, y: 160 }
+  }
+};
+
+export default JOURNAL;
